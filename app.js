af59667b719f3f037ae3767b45c3dec04ad32321
Vsept17 <verzchseptian@gmail.com>
2020-07-01 18:54:01 +0700

    update

--- app.js
+++ app.js
@@ -3,3 +3,12 @@ const nav = document.querySelector('.menu-list ul')
 menuToggle.addEventListener('click', function(){
     nav.classList.toggle('slide');
 });
+
+const ftProfile = document.querySelector('.ftprofile');
+ftProfile.addEventListener('click', (even) => {
+  if (document.fullscreenElment) {
+    document.exitFullscreen();
+  } else {
+    document.querySelector('.ftprofile').requestFullscreen();
+  }
+})
